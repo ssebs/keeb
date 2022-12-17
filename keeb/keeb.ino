06ec64e971250acda14d2e44fcdd4117e1c2d738
Sebastian Safari <ssebs@ymail.com>
2022-12-16 19:12:24 -0800

    swap modes

--- keeb/keeb.ino
+++ keeb/keeb.ino
@@ -3,8 +3,12 @@
 #define NUM_COLS 3
 #define NUM_ROWS 3
 
+#define MODE_SW_PIN 8
+
 #define DEBOUNCE 2
 #define SCAN_DELAY 200
+
+enum switchMode { NUMPAD = 0, VAL = 1, HELPER = 2, _NUM_MODES = 3};
 
 static const uint8_t ColPins[NUM_COLS] = { 6, 5, 4 };
 static const uint8_t RowPins[NUM_ROWS] = { 3, 2, 1 };
@@ -12,8 +16,31 @@ static uint8_t debounce_count[NUM_ROWS][NUM_COLS];
 uint8_t pos = 0;
 
 const char keyData[9] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+const char *valStrings[10] = {
+  "Sheeeeeeeeeeeeeeeeeeeeeeeeesh",
+  "You were a boulder. I am a mountain!",
+  "How did every piece of trash end up on the same team?",
+  "Just some good old hard yakka, standing in between us and victory.",
+  "Oi! I'm pissed!",
+  "Bloinded",
+  "I am on a higher plane, chale, literally!",
+  "Activating kill mode. That's a joke. Kill mode is default.",
+  "Buy stuff, kaching, lil' skkkrrrr, then we're done, yeah?",
+  "Yo!    Nice."
+};
+const char *valPickupStrings[5] = {
+  "Are you Cypher? Because I'd give you my corpse.",
+  "Are you killjoy? Because i want you to detain me.",
+  "Are you Viper? Because your toxicity has me addicted.",
+  "I wish I were Omen because I want to teleport into your bed tonight.",
+  "You must be be Reyna, because you are sucking more than just my soul.",
+};
+
 static uint8_t currentRow = 0;
 static uint8_t currentCol;  // for column loop counters
+static uint8_t currentMode;
+
+bool lightOn = false;
 
 void setup() {
   Serial.begin(9600);
@@ -23,14 +50,70 @@ void setup() {
   for (i = 0; i < NUM_ROWS; i++) {
     pinMode(RowPins[i], OUTPUT);
     digitalWrite(RowPins[i], HIGH);
+    delay(100);
   }
   // Setup col pins
   for (i = 0; i < NUM_COLS; i++) {
     pinMode(ColPins[i], INPUT_PULLUP);
   }
+
+  // Setup extras
+  pinMode(MODE_SW_PIN, INPUT_PULLUP);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  currentMode = NUMPAD;
 }
 
 void loop() {
+  // Serial.print("Switch Mode: ");
+  // Serial.println(currentMode);
+  
+  // Mode switch
+  if (digitalRead(MODE_SW_PIN) == LOW) {
+    if (currentMode >= _NUM_MODES - 1 ) {
+      currentMode = NUMPAD;
+    } else {
+      currentMode += 1;
+    }
+    if (lightOn) {
+      digitalWrite(LED_BUILTIN, LOW);
+      lightOn = !lightOn;
+    } else {
+      digitalWrite(LED_BUILTIN, HIGH);
+      lightOn = !lightOn;
+    }
+    delay(100);
+  }
+
+  switch (currentMode) {
+    case NUMPAD:
+      handleNumPad();
+      break;
+    case VAL:
+      handleVal();
+      break;
+    case HELPER:
+      handleHelper();
+      break;
+    default:
+      Serial.println("default mode, how'd you get here?");
+      break;      
+  }
+
+  // sendRandomString(valStrings, sizeof(valStrings) / sizeof(valStrings[0]));
+}
+
+void handleHelper() {
+  Serial.println("IN HANDLE HELPER");
+}
+
+void handleVal() {
+  Serial.println("IN HANDLE VAL");
+}
+
+void handleNumPad() {
+  // TODO: pass function to this
+
   // Select current row
   digitalWrite(RowPins[currentRow], LOW);
   // add a slight delay
@@ -38,20 +121,13 @@ void loop() {
 
   // Scan through switches on this row:
   for (currentCol = 0; currentCol < NUM_COLS; currentCol++) {
-
     if (digitalRead(ColPins[currentCol]) == LOW) {
       if (debounce_count[currentRow][currentCol] < DEBOUNCE) {
         debounce_count[currentRow][currentCol]++;
         // If debounce counter hits MAX_DEBOUNCE, trigger key press
         if (debounce_count[currentRow][currentCol] == DEBOUNCE) {
-          Serial.print("ROW:");
-          Serial.println(currentRow);
-          Serial.print(" COL:");
-          Serial.println(currentCol);
-          Serial.println(" PRESSED");
-          Serial.print(pos);
-          Serial.println(" POS");
 
+          // Do stuff
           Keyboard.press(keyData[pos]);
         }
       }
@@ -61,6 +137,8 @@ void loop() {
         // Decrement debounce counter
         debounce_count[currentRow][currentCol]--;
         if (debounce_count[currentRow][currentCol] == 0) {  // If debounce counter hits 0
+
+          // Release key press
           Keyboard.release(keyData[pos]);
         }
       }
@@ -75,59 +153,48 @@ void loop() {
   if (currentRow == 0) pos = 0;
 }
 
+void sendValString() {
+  Keyboard.press(KEY_LEFT_SHIFT);
+  Keyboard.press(KEY_RETURN);
+  delay(100);
+  Keyboard.releaseAll();
 
+  sendRandomString(valStrings, sizeof(valStrings) / sizeof(valStrings[0]));
+}
 
-// #include <Keyboard.h>
+void sendRandomString(const char **strings, int numStrings) {
+  // int idx = random(numStrings);
+  int idx = random(numStrings);
+  Keyboard.println(strings[idx]);  // random str
+  delay(500);
+}
 
-// // Rows
-// #define R0 1
-// #define R1 2
-// #define R2 3
+void sendShutdown() {
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  delay(100);
+  Keyboard.releaseAll();
 
-// // Cols
-// #define C1 6
-// #define C2 5
-// #define C3 4
+  // Keyboard.println("shutdown /s");
+  Keyboard.println("msinfo32");
+}
 
-// bool hasPressed = false;
+void sendOpenIncognito() {
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('1');
+  delay(200);
+  Keyboard.releaseAll();
 
-// void setup() {
-//   // Setup cols
-//   pinMode(R0, OUTPUT);
-//   pinMode(R1, OUTPUT);
-//   pinMode(R2, OUTPUT);
-//   digitalWrite(R0, LOW);
-//   digitalWrite(R1, LOW);
-//   digitalWrite(R1, LOW);
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press(KEY_LEFT_SHIFT);
+  Keyboard.press('n');
+  delay(100);
+  Keyboard.releaseAll();
+}
 
-//   // Rows
-//   pinMode(C1, INPUT_PULLUP);
-//   pinMode(C2, INPUT_PULLUP);
-//   pinMode(C3, INPUT_PULLUP);
-
-// }
-
-// void loop() {
-//   delay(200);
-
-//   for (int i = 1;  i < 4; i++) {
-//     if (hasPressed) {
-//       hasPressed = false;
-//       break;
-//     }
-//     // Set row val temporarily
-//     digitalWrite(i, HIGH);
-
-//     if (digitalRead(C1) == LOW) {
-//       hasPressed = true;
-//       Serial.print("Pressed row: ");
-//       Serial.print(i);
-//       Serial.print(", col: 1\n");
-//     }
-//     if (digitalRead(C2) == LOW) {
-//       Serial.println("pressed c1 r0");
-//     }
-
-//     digitalWrite(i, LOW);
-//   }
-// }
+void sendUndo() {
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press('z');
+  delay(100);
+  Keyboard.releaseAll();
+}
