14d10564a4353dd5ff82dfc9da25ac62dbd0d346
Sebastian Safari <ssebs@ymail.com>
2022-12-20 18:31:47 -0800

    serial comms

---
+++ client/notify.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# notify.py - Notify user of keyboard mode switches
+
+import time
+import signal
+import sys
+
+import serial
+import serial.tools.list_ports
+from win10toast import ToastNotifier
+
+from util import switchMode
+
+DEBUG = False
+
+def main():
+    print("Client")
+    signal.signal(signal.SIGINT, signal.default_int_handler)
+    serial_port = load_port("COM6")
+
+    arduino = serial.Serial(port=serial_port, baudrate=2000000,  timeout=.5)
+    if arduino is None:
+        print("Failed to mount COM6")
+
+    while True:
+        data = str(arduino.readline().decode())
+        if data != "" and DEBUG: 
+            print(data)
+        if data.startswith("mode:"):
+            mode = int(data.split(":")[1].strip())
+            print(switchMode(mode).name)
+
+
+def load_port(name: str) -> str:
+    ports = list(serial.tools.list_ports.comports())
+    if DEBUG:
+        print("Serial Ports:")
+    for p in ports:
+        if DEBUG:
+            print(p.name)
+        if p.name == name:
+            return p.name
+    return None
+
+
+if __name__ == "__main__":
+    main()
---
+++ client/util.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# util.py - Util stuff
+from enum import Enum
+
+class switchMode (Enum):
+    NUMPAD = 0
+    VAL = 1
+    HELPER = 2
+    _NUM_MODES = 3
