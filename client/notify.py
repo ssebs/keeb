93295bff3555770938a68e59c2afe918ab235141
Sebastian Safari <ssebs@ymail.com>
2022-12-28 09:57:54 -0800

    sfx instead of toast

--- client/notify.py
+++ client/notify.py
@@ -8,25 +8,26 @@ import threading
 
 import serial
 import serial.tools.list_ports
-from win10toast import ToastNotifier
+from playsound import playsound
 
 from util import switchMode, MACRO_ITEMS
 from macrodisplay import MacroDisplay
 from tkinter import Tk
 
 DEBUG = True
-SECOND_MONITOR = True
+SECOND_MONITOR = False
 
-ICON_PATH = './img/bell.ico'
+ICON_PATH = './client/res/bell.ico'
+SFX_PATH = './client/res/snap.mp3'
 
 def main():
     print("Client for macro pad")
     signal.signal(signal.SIGINT, signal.default_int_handler)
+    # TODO: Find correct port automatically
     serial_port = load_port("COM7")
     arduino = serial.Serial(port=serial_port, baudrate=9600,  timeout=1.2)
     if arduino is None:
         print("Failed to mount COM7")
-    toast = ToastNotifier()
 
     global root
     root = Tk()
@@ -50,13 +51,14 @@ def main():
     root.geometry(f"456x300+{posX}+{posY}")
 
     global thread1 
-    thread1 = threading.Thread(target=main_loop, args=(arduino, root, macro_display, toast))
+    thread1 = threading.Thread(target=main_loop, args=(arduino, root, macro_display))
+    thread1.daemon = True
     thread1.start()
 
     root.mainloop()
 
 
-def main_loop(arduino, root, macro_display, toast):
+def main_loop(arduino, root, macro_display):
     while True:
         data = str(arduino.readline().decode())
         if data != "" and DEBUG:
@@ -74,8 +76,7 @@ def main_loop(arduino, root, macro_display, toast):
             mode = int(data.split(":")[1].strip())
             # print(switchMode(mode).name)
             macro_display.update_mode(switchMode(mode).name)
-            toast.show_toast("Keyboard mode:", switchMode(
-                mode).name, duration=2, icon_path=ICON_PATH)
+            playsound(SFX_PATH, False)
 
         if data.startswith("log:"):
             print(data)
@@ -87,7 +88,7 @@ def load_port(name: str) -> str:
         print("Serial Ports:")
     for p in ports:
         if DEBUG:
-            print(p.name)
+            print(f"{p.name} - {p.description}")
         if p.name == name:
             return p.name
     return None
